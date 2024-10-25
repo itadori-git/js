be89346c5f41ebfd2e0aea52f7925cf996a7d25b
Mishan Ghale Gurung <gurungmishan22@gmail.com>
2024-10-25 09:34:31 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Hello world");
+console.log("code is running")
+var a = prompt("Enter your number")
+console.log("your number is " + a)
+var isTrue = confirm("Are you sure want to leave this page and blast your computer ")
+if(isTrue){
+    console.log("computer is blasting ")
+}
+else{
+    console.log("computer is not blasting")
+}
